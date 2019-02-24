ee74c3ed53f4e27f44adbbf237fc78c45f35f49e
Juan Minnocci <juan@monoqi.de>
2019-02-24 03:06:51 +0100

    Remove async actions tests

--- src/tests/actions.user.test.js
+++ src/tests/actions.user.test.js
@@ -1,11 +1,6 @@
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-
 import {
   loginUser,
-  logoutUser,
-  handleLoginUser,
-  handleLogoutUser
+  logoutUser
 } from '../actions/user'
 
 import {
@@ -16,9 +11,6 @@ import {
 import {
   meMock as user
 } from '../utils/_DATA'
-
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
 
 describe('user actions', () => {
 
@@ -39,38 +31,6 @@ describe('user actions', () => {
       userId
     }
     expect(logoutUser(userId)).toEqual(expectedAction)
-  })
-
-  describe('async handleLoginUser', () => {
-
-    it('should dispatch loginUser when handleLoginUser has been done', () => {
-      const { user_id: userId, access_type: accessType } = user
-      const expectedActions = [
-        { type: LOGIN_USER, userId, accessType }
-      ]
-      const store = mockStore({})
-      return store.dispatch(handleLoginUser()).then(() => {
-        // return of async actions
-        expect(store.getActions()).toEqual(expectedActions)
-      })
-    })
-
-  })
-
-  describe('async handleLogoutUser', () => {
-
-    it('should dispatch logoutUser when handleLogoutUser has been done', () => {
-      const { user_id: userId } = user
-      const expectedActions = [
-        { type: LOGOUT_USER, userId }
-      ]
-      const store = mockStore({})
-      return store.dispatch(handleLogoutUser(userId)).then(() => {
-        // return of async actions
-        expect(store.getActions()).toEqual(expectedActions)
-      })
-    })
-
   })
 
 })
